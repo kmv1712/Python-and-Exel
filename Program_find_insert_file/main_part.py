56c5b7b03ae210a4e13cdf4837b130e52720ebd2
kmv1712 <kmv1712@gmail.com>
2018-08-10 20:53:47 +0500

    edit100818

--- Program_find_insert_file/main_part.py
+++ Program_find_insert_file/main_part.py
@@ -115,12 +115,12 @@ open_main_task()
 
 # rb = xlrd.open_workbook('Уралтест.xlsx')
 
-#выбираем активный лист
-# sheet = rb.sheet_by_index(0)
-# i = 0
-# while i < 1000:
-#     i += 1
-#     cell = sheet.row_values(i)[0]
-#     if cell.value:
-#         print (i)
-#         break
+выбираем активный лист
+sheet = rb.sheet_by_index(0)
+i = 0
+while i < 1000:
+    i += 1
+    cell = sheet.row_values(i)[0]
+    if cell.value:
+        print (i)
+        break
